a4c45b5588ade341d31c004eece78dd03135385a
Hope <51793979+rohannY@users.noreply.github.com>
2023-04-29 02:46:36 +0530

    Updated user model

--- models/user.model.js
+++ models/user.model.js
@@ -62,7 +62,7 @@ userSchema.methods.createResetPassLink = function () {
   const token = jwt.sign(payload, secret, {
     expiresIn: process.env.RESET_PASS_EXPIRE,
   });
-  return `http://localhost:3000/reset/${this._id}/${token}`;
+  return `http://localhost:3000/reset?id=${this._id}&token=${token}`;
 };
 userSchema.methods.verifyResetToken = function (token) {
   const secret = process.env.JWT_SECRET + this.password;
