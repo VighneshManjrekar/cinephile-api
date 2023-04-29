b1fcec5c1fd3c90b573531c9c05f813e05220693
Vighnesh Manjrekar <vighneshmanjrekar4193@gmail.com>
2023-04-29 12:18:28 +0530

    Update sendmail method

--- utils/mail.js
+++ utils/mail.js
@@ -1,28 +1,20 @@
 const nodemailer = require("nodemailer");
-const sgMail = require("@sendgrid/mail")
+
 const sendMail = async ({ to, subject, text }) => {
   const message = {
-    from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
+    from: `${process.env.SMTP_FROM_NAME} <${process.env.GMAIL_ID}>`,
     to,
     subject,
     text,
   };
-  if (process.env.NODE_ENV == "production") {
-    // for production use sendgrid
-    sgMail.setApiKey(process.env.SENDGRID_PASS);
-    await sgMail.send(message);
-  } else {
-    // for development use mailtrap
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
-    await transporter.sendMail(message);
-  }
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_ID,
+      pass: process.env.GMAIL_PASS,
+    },
+  })
+  await transporter.sendMail(message);
 };
 
 module.exports = sendMail;
